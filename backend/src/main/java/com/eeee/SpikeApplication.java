a44dadcb8fce36f3e76563c871613d4e31b467fd
Charles <charles.qld.au@gmail.com>
2017-07-26 23:34:32 +1000

    Enable web security

--- backend/src/main/java/com/eeee/SpikeApplication.java
+++ backend/src/main/java/com/eeee/SpikeApplication.java
@@ -1,12 +1,82 @@
 package com.eeee;
 
+import com.eeee.services.AccountService;
+import com.eeee.spike.model.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import javax.management.MXBean;
+import java.util.Optional;
 
 @SpringBootApplication
 public class SpikeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpikeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpikeApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(final AccountService accountService) {
+
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... arg0) throws Exception {
+                accountService.add(new Account("Shi", "Charles", "charles.qld@gmail.com", "abcd1234"));
+            }
+
+        };
+    }
+}
+
+
+@Configuration
+class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
+
+    @Autowired
+    AccountService accountService;
+
+    @Override
+    public void init(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService());
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+                Optional<Account> account = accountService.getByEmail(s);
+                if (account.isPresent()) {
+                    return new User(account.get().getEmail(), account.get().getPassword(), true, true, true, true, AuthorityUtils.createAuthorityList("USER"));
+                } else {
+                    throw new UsernameNotFoundException("Could not find user '" + s + "'");
+                }
+            }
+        };
+    }
+}
+
+@EnableWebSecurity
+@Configuration
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().fullyAuthenticated().and().httpBasic().and().csrf().disable();
+    }
 }
